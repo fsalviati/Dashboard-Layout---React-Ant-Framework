ff4ba8c407c81bed01adec226054d1c52846039b
Fernando Salviati <fernandosalviati@gmail.com>
2021-06-08 11:33:26 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Tree } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import Avatar from 'antd/lib/avatar/avatar';
-import { MailOutlined } from '@ant-design/icons';
+import {
+  MailOutlined,
+  UsergroupAddOutlined,
+  DownloadOutlined,
+  CloudDownloadOutlined,
+  DownOutlined,
+  FrownOutlined,
+  FrownFilled,
+  MenuFoldOutlined,
+} from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 const { Header, Footer, Sider, Content } = Layout;
+
+const treeData = [
+  {
+    title: '',
+    key: '0-0',
+    children: [
+      {
+        title: 'Leaf 1',
+        key: '0-0-0',
+      },
+      {
+        title: 'Leaf 2',
+        key: '0-0-1',
+      },
+    ],
+  },
+];
 
 function App() {
   return (
@@ -17,23 +43,34 @@ function App() {
           <Title style={{ color: 'white' }} level={3}>Dashboard</Title>
         </Header>
         <Layout>
-          <Sider className='sider'>
+          <Sider className='sider' breakpoint="sm" collapsedWidth="150" >
             <Menu
               defaultSelectedKeys={['Dashboard']}
               mode="inline"
             >
-              <Menu.Item key='Dashboard'>
+              <Menu.Item key='Dashboard' breakpoint="sm">
                 Dashboard
               </Menu.Item>
               <SubMenu icon={<MailOutlined />}
                 title={
                   <span>
-                    <span>Section 1</span>
+                    <span>Main Section</span>
                   </span>
                 }>
-                <Menu.ItemGroup key='AboutUS' title='Country'>
-                  <Menu.Item key='location1'>Sub-section 1</Menu.Item>
-                  <Menu.Item key='location2'>Sub-section 2</Menu.Item>
+                <Menu.ItemGroup key='section1' title='Section 1'>
+                  <Menu.Item key='sub1Section1' breakpoint="sm" icon={<UsergroupAddOutlined />}><span className="menuStyle">Sub-section 1</span></Menu.Item>
+                  <Menu.Item key='sub1Section2' breakpoint="sm" icon={<DownloadOutlined />}><span className="menuStyle">Sub-section 2</span></Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+              <SubMenu icon={<CloudDownloadOutlined />}
+                title={
+                  <span>
+                    <span>Second Section</span>
+                  </span>
+                }>
+                <Menu.ItemGroup key='section2' title='Section 2'>
+                  <Menu.Item key='sub2Section1' breakpoint="sm" icon={<UsergroupAddOutlined />}><span className="menuStyle">Sub-section 1</span></Menu.Item>
+                  <Menu.Item key='sub2Section2' breakpoint="sm" icon={<DownloadOutlined />}><span className="menuStyle">Sub-section 2</span></Menu.Item>
                 </Menu.ItemGroup>
               </SubMenu>
             </Menu>
@@ -41,7 +78,13 @@ function App() {
           <Layout>
             <Content style={{ padding: '0 50px' }}>
               <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+                <Tree
+                  showIcon
+                  defaultSelectedKeys={['0-0-0']}
+                  switcherIcon={<MenuFoldOutlined style={{ fontSize: '22px' }} />}
+                  treeData={treeData}
+                />
+
               </Breadcrumb>
               <div className="site-layout-content">Content</div>
             </Content>
